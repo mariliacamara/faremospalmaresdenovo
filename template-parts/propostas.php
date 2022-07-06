b78e68a321b283f9cd843d27b29d2ac492fff35e
Marilia <marilia@ibm.com>
2022-07-05 23:23:54 -0300

    chore: change html structure

--- template-parts/propostas.php
+++ template-parts/propostas.php
@@ -1,13 +1,19 @@
 <div class="col-md-4 pt-3 text-center">
   <div class="p-3 border bg-light proposal-box">
-    <h2 class="proposal-title"><?php the_title(); ?></h2>
-    <h3 class="display-5">Enviada por <span>
-        <?php echo get_post_meta($post->ID, "your_name", true); ?></span></h3>
+    <div class="proposal-header">
+      <h2 class="proposal-title"><?php the_title(); ?></h2>
+      <h3 class="display-5">
+        Enviada por <span><?php echo get_post_meta($post->ID, "your_name", true); ?></span>
+      </h3>
+    </div>
     <p>
       <?php
-        echo mb_strimwidth(strip_tags(get_the_content()), 0, 150, "..."); 
+        echo mb_strimwidth(strip_tags(get_the_content()), 0, 140, "..."); 
       ?>
-    <p><a class="btn btn-secondary" href="<?php echo esc_url( get_permalink() ); ?>" role="button">Ver detalhes
-        &raquo;</a></p>
+    </p>
+    <p>
+      <a class="btn btn-secondary" href="<?php echo esc_url( get_permalink() ); ?>" role="button">Ver detalhes
+        &raquo;</a>
+    </p>
   </div>
 </div>
