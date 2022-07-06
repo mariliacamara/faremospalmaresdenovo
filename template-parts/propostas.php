439139e85c06c67d759f6df15a53952deb26ffb6
Marilia <marilia@ibm.com>
2022-07-05 23:03:32 -0300

    feat: implement content limitador to proposal cards

--- template-parts/propostas.php
+++ template-parts/propostas.php
@@ -1,8 +1,13 @@
-<div class="col-md-4 text-center">
-  <div class="p-3 border bg-light">
+<div class="col-md-4 pt-3 text-center">
+  <div class="p-3 border bg-light proposal-box">
     <h2 class="proposal-title"><?php the_title(); ?></h2>
     <h3 class="display-5">Enviada por <span>
         <?php echo get_post_meta($post->ID, "your_name", true); ?></span></h3>
-    <p><?php echo the_content(); ?></p>
+    <p>
+      <?php
+        echo mb_strimwidth(strip_tags(get_the_content()), 0, 150, "..."); 
+      ?>
+    <p><a class="btn btn-secondary" href="<?php echo esc_url( get_permalink() ); ?>" role="button">Ver detalhes
+        &raquo;</a></p>
   </div>
 </div>
