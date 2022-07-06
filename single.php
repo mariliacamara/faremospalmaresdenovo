3cdc72a3409d49616d2968c635ea34106cf4eaf4
Marilia <marilia@ibm.com>
2022-07-05 23:04:17 -0300

    chore: remove image from single page

--- single.php
+++ single.php
@@ -1,13 +1,10 @@
 <?php get_header(); ?>
 
-<section id="post_reportagem">
+<section>
   <?php 
     if( have_posts() ):
     while( have_posts() ): the_post();
   ?>
-  <div class="post-image">
-    <?php the_post_thumbnail( 'large', array( 'class' => 'img-fluid' ) ); ?>
-  </div>
   <div class="container">
     <div class="post-inner ml-5 mr-5">
       <h1 class="post-title"><?php the_title( ) ?></h1>
